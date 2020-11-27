2eaa6248ffc124bc244886b0ad591c31e07f7d6c
mr-keep-it-simple <72211500+mr-keep-it-simple@users.noreply.github.com>
2020-11-27 16:28:29 +0000

    cleaned b64toBlob

--- cloudstore.js
+++ cloudstore.js
@@ -174,19 +174,15 @@ class CloudStore
         try {
             const byteCharacters = atob(b64Data);
             const byteArrays = [];
-
             for (let offset = 0; offset < byteCharacters.length; offset += sliceSize) {
-            const slice = byteCharacters.slice(offset, offset + sliceSize);
-
-            const byteNumbers = new Array(slice.length);
-            for (let i = 0; i < slice.length; i++) {
-                byteNumbers[i] = slice.charCodeAt(i);
-            }
-
-            const byteArray = new Uint8Array(byteNumbers);
-            byteArrays.push(byteArray);
-            }    
-            console.log(byteArrays);
+                const slice = byteCharacters.slice(offset, offset + sliceSize);
+                const byteNumbers = new Array(slice.length);
+                for (let i = 0; i < slice.length; i++) {
+                    byteNumbers[i] = slice.charCodeAt(i);
+                }
+                const byteArray = new Uint8Array(byteNumbers);
+                byteArrays.push(byteArray);
+            }            
             const blob = new File(byteArrays, fileName, {type: contentType});
             return blob;
         } catch(error) {
